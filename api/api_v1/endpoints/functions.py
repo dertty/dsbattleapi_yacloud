69e204a9f2936442bdcbe833589c426dc2411774
Aleksandr <alex.iv.vad@gmail.com>
2022-07-28 12:05:55 +0300

    star submit

--- api/api_v1/endpoints/functions.py
+++ api/api_v1/endpoints/functions.py
@@ -319,7 +319,7 @@ def change_username(user_details: schemas.UserInfo, db: Session = Depends(get_db
 def star_submit(sid: int, uid: int, action: int, hid: int, db: Session = Depends(get_db), access_token: Optional[str] = ''):
     check_access_token(access_token)
     utc = pytz.UTC
-    if hid == 10 or hid== 8:
+    if hid == 10 or hid == 5:
         if action == 0:
             flags_num = submits.count_submit_star_flags(db=db, uid=uid, hid=hid)
             if flags_num is not None:
