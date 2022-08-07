de86d168c742629a998154b774ab1b7bf97bd949
Aleksandr <alex.iv.vad@gmail.com>
2022-08-07 20:37:07 +0300

    star submit

--- api/api_v1/core/models/crud/submits.py
+++ api/api_v1/core/models/crud/submits.py
@@ -618,7 +618,7 @@ def get_final_leaderboard_mkb(
     pre_leaderboard = crud.get_submits(db=db, query=True) \
         .filter(models.SubmitOld.hid == hid) \
         .filter(models.SubmitOld.bid == bid) \
-        .filter(models.SubmitOld.submit_dt < '2022-08-01') \
+        .filter(models.SubmitOld.submit_dt < '2022-08-08') \
         .with_entities(
             models.SubmitOld.uid,
             models.SubmitOld.public_score,
