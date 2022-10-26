74f91abe5a0c60cc25cbcb85ea551858ffd1b47b
Aleksandr <alex.iv.vad@gmail.com>
2022-10-26 08:07:17 +0300

    addded new db

--- api/api_v1/core/database.py
+++ api/api_v1/core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from .credentials import SQL_LOGIN, SQL_PASSWORD
 
 
-SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{SQL_LOGIN}:{SQL_PASSWORD}@dsbatttle.cjf9z3vqvye8.us-east-2.rds.amazonaws.com/gb_bkhv'
+SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{SQL_LOGIN}:{SQL_PASSWORD}@dsbattle.cjf9z3vqvye8.us-east-2.rds.amazonaws.com/gb_bkhv'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
